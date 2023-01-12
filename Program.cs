262a5ffb3f0d2294d3df5480920853f94ace140c
Темнова <nastena_petlilev@mail.ru>
2023-01-12 20:04:46 +0400

    Программа

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// Написать программу, которая из имеющегося массива строк формирует новый массив из строк, 
+//длина которых меньше, либо равна 3 символам. 
+string[] arrayStart = new string[13] {"Hello", "Cat", "!", "Hi", "Dog", "Home", "12579", "English", "Tea", "Coffee", "Bat", "Book", "Bee"};
+string[] arrayFinal = new string[arrayStart.Length];
+void ShortArray(string[] arrayStart, string[] arrayFinal)
+{
+    int count = 0;
+    for (int i = 0; i < arrayStart.Length; i++)
+    {
+    if(arrayStart[i].Length <= 3)
+        {
+        arrayFinal[count] = arrayStart[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+ShortArray(arrayStart, arrayFinal);
+PrintArray(arrayFinal);
